06d9dd522abe7f273e4171ea2c82450ee8846f53
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-01-22 12:06:40 +0300

    Create 77.js

---
+++ Basic JS/77.js
@@ -0,0 +1,25 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    case "d":
+      return "stuff";
+    default:
+      return "stuff";
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
