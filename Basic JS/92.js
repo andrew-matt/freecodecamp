5ddc741ff39f86d5ca8d388ff4149e0f1e073d72
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-01-23 11:18:17 +0300

    basic

---
+++ Basic JS/92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Till Lindemann",
+    "title": "The best",
+    "release_year": 2001,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
